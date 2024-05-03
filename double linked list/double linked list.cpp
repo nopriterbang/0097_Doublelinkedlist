994b2b51a03d124eebc9b81e5e88863d5d8f6c79
nopriterbang <novalpriya@gmail.com>
2024-05-03 13:53:43 +0700

    Membuat tampilan dengan cin dan cout

--- double linked list/double linked list.cpp
+++ double linked list/double linked list.cpp
@@ -13,5 +13,9 @@ Node* START = NULL;
 
 Void addnode()
 {
-
+	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
 }
